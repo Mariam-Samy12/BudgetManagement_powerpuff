5bb17f2c67ff856b56cb399ed5aedf0363d5c9b3
alaa <lolomamdo2006@gmail.com>
2026-05-07 01:08:28 +0300

    validation:

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Account
-
+from django.core.exceptions import ValidationError
+import re  
 class SignupForm(forms.ModelForm):
     username = forms.CharField(
         widget=forms.TextInput(attrs={
@@ -15,7 +16,7 @@ class SignupForm(forms.ModelForm):
         widget=forms.PasswordInput(attrs={
             'class': 'password',
             'id': 'password',
-            'minlength': '10',
+            'minlength': '8',
             
         })
     )
@@ -39,3 +40,19 @@ class SignupForm(forms.ModelForm):
     class Meta:
         model = Account
         fields = ['username', 'email', 'password']
+    def clean_password(self):
+        password=self.cleaned_data.get('password')
+        if not re.search(r'[0-9]', password):
+            raise ValidationError("Password must contain at least one digit (0-9).")
+        if not re.search(r'[A-Z]',password):
+            raise ValidationError("Password must contain at least one uppercase letter (A-Z).")
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
+            raise ValidationError("Password must contain at least one special character (e.g., @, #, $, %).")
+        return password
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+        if password and confirm_password and password != confirm_password:
+            self.add_error('confirm_password', "Passwords do not match. Please try again.")
+        return cleaned_data
